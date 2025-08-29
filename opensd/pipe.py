131aef2ef84a3cc747e79c2f3463cae95eaa894a
vikramgovindarajan <vickymailbuddy@gmail.com>
2025-08-29 07:12:50 +0530

    remove internal node creation in XML input

--- opensd/pipe.py
+++ opensd/pipe.py
@@ -38,39 +38,39 @@ class Pipe(object):
         self.heat_input = heat_input
         self.roughness = roughness
 
-        self.nodes = []
-        for i in range(ncell-1):
-            node=circuit.add_node(identifier+"_node"+str(i),volume=delx*cfarea,elevation=unode.elevation+(dnode.elevation-unode.elevation)*(i+1)/ncell)
-            node.i = i
-            self.nodes.append(node)
+        # self.nodes = []
+        # for i in range(ncell-1):
+            # node=circuit.add_node(identifier+"_node"+str(i),volume=delx*cfarea,elevation=unode.elevation+(dnode.elevation-unode.elevation)*(i+1)/ncell)
+            # node.i = i
+            # self.nodes.append(node)
         
-        self.faces = []
-        for i in range(ncell):
-            if i==0 and ncell==1:
-                self.faces.append(PFace(i,self,unode,ufrac,dnode,dfrac,diameter,cfarea,delx,delz,fricopt,roughness))
-            elif i==0:
-                self.faces.append(PFace(i,self,unode,ufrac,self.nodes[0],None,diameter,cfarea,delx,delz,fricopt,roughness))
-            elif i==ncell-1:
-                self.faces.append(PFace(i,self,self.nodes[ncell-2],None,dnode,dfrac,diameter,cfarea,delx,delz,fricopt,roughness))
-            else:
-                self.faces.append(PFace(i,self,self.nodes[i-1],None,self.nodes[i],None,diameter,cfarea,delx,delz,fricopt,roughness))
+        # self.faces = []
+        # for i in range(ncell):
+            # if i==0 and ncell==1:
+                # self.faces.append(PFace(i,self,unode,ufrac,dnode,dfrac,diameter,cfarea,delx,delz,fricopt,roughness))
+            # elif i==0:
+                # self.faces.append(PFace(i,self,unode,ufrac,self.nodes[0],None,diameter,cfarea,delx,delz,fricopt,roughness))
+            # elif i==ncell-1:
+                # self.faces.append(PFace(i,self,self.nodes[ncell-2],None,dnode,dfrac,diameter,cfarea,delx,delz,fricopt,roughness))
+            # else:
+                # self.faces.append(PFace(i,self,self.nodes[i-1],None,self.nodes[i],None,diameter,cfarea,delx,delz,fricopt,roughness))
             
-        for i in range(ncell-1):
-            self.nodes[i].ifaces.append(self.faces[i])
-            self.nodes[i].ofaces.append(self.faces[i+1])
+        # for i in range(ncell-1):
+            # self.nodes[i].ifaces.append(self.faces[i])
+            # self.nodes[i].ofaces.append(self.faces[i+1])
 
-        unode.ofaces.append(self.faces[0])
+        # unode.ofaces.append(self.faces[0])
         unode.volume = unode.volume + 0.5*delx*cfarea
 
-        dnode.ifaces.append(self.faces[-1])
+        # dnode.ifaces.append(self.faces[-1])
         dnode.volume = dnode.volume + 0.5*delx*cfarea
         
         self.mflow = 0.
         
     def add_wall(self,thk,solname,sollib,restraint):
         wall = Wall(thk,solname,sollib,restraint)
-        for face in self.faces:
-            face.wall = wall
+        # for face in self.faces:
+            # face.wall = wall
 
     def to_xml_element(self,element):
         subelement = ET.SubElement(element, "pipe")
