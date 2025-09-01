b2f1d22d1e9158a7fbed9773c0f9eeda91c73860
vikramgovindarajan <vickymailbuddy@gmail.com>
2025-09-01 07:55:52 +0530

    tutorial6 secondary sodium circuit added and converged

--- tutorials/tutorial6/tutorial.py
+++ tutorials/tutorial6/tutorial.py
@@ -2,25 +2,42 @@
 
 import opensd
 
-#primary circuit (IHX shell)
-circuit1 = opensd.Circuit(identifier="circuit1")
-circuit1.assign_fluid(flname="LiqNa",fltype="incompressible") #fluid name as per CoolProp
-node1 = circuit1.add_node("node1")
-node2 = circuit1.add_node("node2")
-
 import math
 
-Af = 0.25*math.pi*1.831**2 - 3600*0.25*math.pi*0.019**2
-Pw = math.pi*(1.831+3600*0.019)
-dh = 4.*Af/(Pw)
+# #primary circuit (IHX shell)
+# circuit1 = opensd.Circuit(identifier="circuit1")
+# circuit1.assign_fluid(flname="LiqNa",fltype="incompressible") #fluid name as per CoolProp
 
-pipe1 = circuit1.add_pipe("pipe1",dh,7.5,"node1","node2",'DW',30.,20,cfarea=Af) #npar=1
+# node1 = circuit1.add_node("node1")
+# node2 = circuit1.add_node("node2")
 
-circuit1.add_BC("bc1","node1",'P',5.E5)
-circuit1.add_BC("bc2","node1",'T',817.)
-circuit1.add_BC("bc3","node2",'msource',-1644.)
+# Af = 0.25*math.pi*1.831**2 - 3600*0.25*math.pi*0.019**2
+# Pw = math.pi*(1.831+3600*0.019)
+# dh = 4.*Af/(Pw)
 
-geometry = opensd.Geometry([circuit1])
+# pipe1 = circuit1.add_pipe("pipe1",dh,7.5,"node1","node2",'DW',30.,20,cfarea=Af) #npar=1
+
+# circuit1.add_BC("bc1","node1",'P',5.E5)
+# circuit1.add_BC("bc2","node1",'T',817.)
+# circuit1.add_BC("bc3","node2",'msource',-1644.)
+
+
+#secondary circuit (IHX tube)
+circuit2 = opensd.Circuit(identifier="circuit2")
+circuit2.assign_fluid(flname="LiqNa",fltype="incompressible")
+
+node3 = circuit2.add_node("node3")
+node4 = circuit2.add_node("node4")
+
+pipe2=circuit2.add_pipe("pipe2",0.0174,7.5,"node3","node4",'DW',30.,20,cfarea=3600*math.pi*0.0174**2/4)
+
+circuit2.add_BC("bc4","node3",'P',5.E5)
+circuit2.add_BC("bc5","node3",'T',628.)
+circuit2.add_BC("bc6","node4",'msource',-1461.)
+
+
+
+geometry = opensd.Geometry([circuit2])
 geometry.export_to_xml()
 
 settings = opensd.Settings()
